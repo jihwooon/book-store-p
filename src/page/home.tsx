1c16ce40243cc56ba28411bb377918b68017b3b5
jihwooon <jihwooon@gmail.com>
2025-02-15 11:58:03 +0900

    버튼 컴포넌트 추가 및 홈 페이지에 적용
    버튼 컴포넌트를 추가하고 홈 페이지에 적용했습니다. 버튼 스타일과 테마 설정도 함께 추가되었습니다.
    
    주요 변경사항:
    1. `Button.tsx` 파일 추가: 버튼 컴포넌트 구현
    2. `home.tsx` 파일 수정: 버튼 컴포넌트 적용
    3. `theme.ts` 파일 수정: 버튼 스타일 및 테마 설정 추가

--- src/page/home.tsx
+++ src/page/home.tsx
@@ -1,10 +1,12 @@
 import styled from "styled-components";
 import Title from "../components/common/title";
+import Button from "../components/common/Button";
 
 const Home = () => {
   return (
     <HomeStyle>
       <Title size="large" color="secondary">제목 테스트</Title>
+      <Button size="large" scheme="primary" disabled={false}>버튼 테스트</Button>
       <div>Header Text</div>
     </HomeStyle>
   );
