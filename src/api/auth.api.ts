459560f96b581e78fbd0aa3174241635f1945dce
jihwooon <jihwooon@gmail.com>
2025-02-17 20:34:49 +0900

    회원가입 API 함수 수정
    회원가입 API 함수에서 실제 HTTP 요청을 주석 처리하고, 사용자 데이터를 반환하도록 수정했습니다.
    
    주요 변경사항:
    1. `httpClient.post` 요청을 주석 처리했습니다.
    2. 사용자 데이터 객체를 반환하도록 함수 내용을 수정했습니다.

--- src/api/auth.api.ts
+++ src/api/auth.api.ts
@@ -1,8 +1,10 @@
 import { SignupProps } from "../page/signup"
-import { httpClient } from "./http"
 
 export const signup = async(userData: SignupProps) => {
-  const response = await httpClient.post("/users/join", userData)
+  // const response = await httpClient.post("/users/join", userData)
 
-  return response.data;
+  return {
+    email: userData.email,
+    password: userData.password
+  };
 }
